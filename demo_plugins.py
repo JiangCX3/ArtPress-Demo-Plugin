75120aea43b1c6fa3b8cac9239adc1c578ff93c2
JiangCX3 <lazyfox158@outlook.com>
2019-12-20 23:25:53 +0800

    Create Blueprint

--- demo_plugins.py
+++ demo_plugins.py
@@ -4,8 +4,8 @@ import artpress_api
 class View:
     def __init__(self):
         PanelMenu = (
-            ["To Baidu", artpress_api.JunpTo.url("www.baidu.com")],
-            ["Demo Options", artpress_api.JunpTo.plugins_options()],
+            ["To Baidu", "icon.png", artpress_api.JunpTo.url("www.baidu.com")],
+            ["Demo Options", "icon.png", artpress_api.JunpTo.plugins_options()],
         )
 
     def adminPage(self):
